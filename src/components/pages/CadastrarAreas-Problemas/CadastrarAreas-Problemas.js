ee853a6619b9a7871cd658a52da1414957eef1d7
adeonita <adeonita.sousa@gmail.com>
2019-08-11 22:36:27 -0300

    Criação da tela de cadastro Area-Problema

---
+++ src/components/pages/CadastrarAreas-Problemas/CadastrarAreas-Problemas.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Component } from 'react';
+import '../../../App.css';
+import FormCadastrarAreaProblema from '../../components/FormCadastrarArea-Problema';
+import FooterLeft from '../../components/FooterLeft';
+import FooterRight from '../../components/FooterRight';
+
+class CadastrarAreaProblema extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Cadastrar Area Problema</h1>
+                <FormCadastrarAreaProblema />
+                <div className="footer-cadastrar-area-problema">
+                  <FooterRight color="#0F3882" />
+                  <FooterLeft color="#F22B2B" />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default CadastrarAreaProblema;
