91e777770239221d2c2609bbe4362888e9cb161a
adeonita <adeonita.sousa@gmail.com>
2019-08-16 23:59:13 -0300

    Dinamização do input cadastrar setor

--- src/components/components/FormCadastrarSetor.js
+++ src/components/components/FormCadastrarSetor.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import { Component } from "react";
+import React, { Component } from 'react';
 import './FormCadastrarSetor.css';
 import { Card, Row, Col, Container, Form, Button } from 'react-bootstrap';
 
+const styleButton = {
+  'backgroundColor': 'white',
+  'borderColor': 'white',
+  'color': '#808080',
+  'margin-left': '-28px',
+  'focus': ''
 
-class FormCadastrarAreaProblema extends Component {
+}
+
+const styleRow = {
+  'marginLeft': '140px'
+}
+
+class FormCadastrarSetor extends Component {
 
     constructor(props){
       super(props);
       this.state = {
                   input: false,
-                  classHide: 'd-none'
+                  classHide: 'd-none',
+                  items: [], //Array para mostrar itens inseridos
+                  text: ''
                 };
       this.addInput = this.addInput.bind(this);
       this.changeClassHide = this.changeClassHide.bind(this);
@@ -44,15 +57,26 @@ class FormCadastrarAreaProblema extends Component {
         return(
           <div className="wow fadeInDown">
           <Container>
-              <Row className="justify-content-center " id="margin-top-bottom">
-                <Col md={5} className="text-center">
-                    <Button onClick={this.addInput} >+ Cadastrar um novo setor</Button>
-                      <Form className = {this.state.classHide}>
-                        <Form.Group controlId="formBasicText">
-                          <Form.Control type="text" placeholder="Setor" />
-                        </Form.Group>
-                      </Form>
-                    </Col>
+              <Row className="justify-content-center" id="margin-top-bottom">
+                <Col md={4} className="text-center">
+                    <Button onClick={this.addInput} style = {styleButton}>
+                      + Cadastrar um novo setor
+                    </Button>
+                </Col>
+              </Row>
+              <Row className="justify-content-center" style = {styleRow}>
+                <Col md={3}>
+                  <Form className = {this.state.classHide}>
+                    <Form.Group controlId="formBasicText" size = "sm">
+                      <Form.Control type="text" placeholder="Setor" />
+                    </Form.Group>
+                  </Form>
+                </Col>
+                <Col md={3}>
+                  <Button className ={this.state.classHide + " "} style = {styleButton} variant="outline-primary" type="submit">
+                   Cadastrar
+                  </Button>
+                </Col>
               </Row>
           </Container>
        </div>
@@ -61,4 +85,4 @@ class FormCadastrarAreaProblema extends Component {
     }
 }
 
-export default FormCadastrarAreaProblema;
+export default FormCadastrarSetor;
